486bfd7fe2d59fb5e980281d53454e2e4cfc09ad
kdb0074 <110100310+kdb0074@users.noreply.github.com>
2023-06-02 15:29:07 +0530

    s2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ function Footer() {
     <>
       <footer>
         <div className="footer-links">
-          <a href="#">About</a>
-          <a href="#">Store locator</a>
-          <a href="#">FAQs</a>
-          <a href="#">News</a>
-          <a href="#">Careers</a>
-          <a href="#">Contact Us</a>
+          <a href="https://github.com/kdb0074">About</a>
+          <a href="https://github.com/kdb0074">Store locator</a>
+          <a href="https://github.com/kdb0074">FAQs</a>
+          <a href="https://github.com/kdb0074">News</a>
+          <a href="https://github.com/kdb0074">Careers</a>
+          <a href="https://github.com/kdb0074">Contact Us</a>
         </div>
         <p className="love">
           Design &nbsp; by{" "}
@@ -18,7 +18,7 @@ function Footer() {
             target="_blank"
             rel="noreferrer"
             style={{ color: "white" }}
-            href="https://github.com/kdb0074/TextEditor"
+            href="https://github.com/kdb0074"
           >
             &nbsp; Kaustubh
           </a>
